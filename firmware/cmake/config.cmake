f53ad78e5834051593851421309469ee4e943872
Kai Raphahn <kai.raphahn@laburec.de>
2019-08-30 19:53:30 +0200

    feat: Create groundwork for the firmware
    remove config.cmake, replace with template
    
    Issue #3

--- firmware/cmake/config.cmake
+++
@@ -1,4 +0,0 @@
-set(ESP8266_ROOT        "C:/Users/kai/AppData/Local/Arduino15/packages/esp8266")
-set(ESP8266_VERSION     "2.5.2")
-set(ESP8266_GCC_VERSION "2.5.0-3-20ed2b9")
-set(ESP8266_GCC_PREFIX  "xtensa-lx106-elf")
