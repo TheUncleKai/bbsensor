638c56e9dd178a57988c81bd6ff9cc8d23d37b75
Kai Raphahn <kai.raphahn@laburec.de>
2019-08-30 19:51:02 +0200

    feat: Create groundwork for the firmware
    change includes
    
    Issue #3

--- firmware/cmake/config.cmake
+++ firmware/cmake/config.cmake
@@ -1,2 +1,4 @@
-set(ESP8266_ROOT  "c:/Program Files (x86)/Arduino")
-set(ESP8266_AVR_ROOT  "${ESP8266_ROOT}/hardware/tools/avr")
+set(ESP8266_ROOT        "C:/Users/kai/AppData/Local/Arduino15/packages/esp8266")
+set(ESP8266_VERSION     "2.5.2")
+set(ESP8266_GCC_VERSION "2.5.0-3-20ed2b9")
+set(ESP8266_GCC_PREFIX  "xtensa-lx106-elf")
