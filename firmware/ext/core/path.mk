14997a9d16d9c305e472c3d85276dde53afabf4a
Kai Raphahn <kai.raphahn@siemens-healthineers.com>
2019-09-19 18:14:41 +0200

    feat: Firmware - Make basics work
    add .cpp/.S stuff
    
    Issue #12

--- firmware/ext/core/path.mk
+++ firmware/ext/core/path.mk
@@ -1,38 +1,21 @@
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 # Object File
 
-OBJS_CORE_ASM := ${SOURCES_CORE_ASM:.S=.o}
+OBJS_CORE_ASM := ${SOURCES_CORE_ASM:.S=.S.o}
 OBJS_CORE_ASM := ${subst $(ESP8266_CORE),$(OBJDIR),$(OBJS_CORE_ASM)}
 
-OBJS_CORE_CPP := ${SOURCES_CORE_CPP:.cpp=.o}
+OBJS_CORE_CPP := ${SOURCES_CORE_CPP:.cpp=.cpp.o}
 OBJS_CORE_CPP := ${subst $(ESP8266_CORE),$(OBJDIR),$(OBJS_CORE_CPP)}
 
-OBJS_LIBB64 := ${SOURCES_LIBB64:.cpp=.o}
+OBJS_LIBB64 := ${SOURCES_LIBB64:.cpp=.cpp.o}
 OBJS_LIBB64 := ${subst $(ESP8266_CORE)/libb64,$(OBJDIR),$(OBJS_LIBB64)}
 
-OBJS_MALLOC := ${SOURCES_MALLOC:.cpp=.o}
+OBJS_MALLOC := ${SOURCES_MALLOC:.cpp=.cpp.o}
 OBJS_MALLOC := ${subst $(ESP8266_CORE)/umm_malloc,$(OBJDIR),$(OBJS_MALLOC)}
 
-OBJS_SPIFFS := ${SOURCES_SPIFFS:.cpp=.o}
+OBJS_SPIFFS := ${SOURCES_SPIFFS:.cpp=.cpp.o}
 OBJS_SPIFFS := ${subst $(ESP8266_CORE)/spiffs,$(OBJDIR),$(OBJS_SPIFFS)}
 
 OBJS := $(OBJS_CORE_ASM) $(OBJS_CORE_CPP) $(OBJS_SPIFFS) $(OBJS_LIBB64) $(OBJS_MALLOC)
 
 ARS := $(OBJS:.o=.ar)
-
-# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# Dependencies
-
-DEP_CORE_CPP := ${SOURCES_CORE_CPP:.cpp=.d}
-DEP_CORE_CPP := ${subst $(ESP8266_CORE),$(OBJDIR),$(DEP_CORE_CPP)}
-
-DEP_LIBB64 := ${SOURCES_LIBB64:.cpp=.d}
-DEP_LIBB64 := ${subst $(ESP8266_CORE)/libb64,$(OBJDIR),$(DEP_LIBB64)}
-
-DEP_MALLOC := ${SOURCES_MALLOC:.cpp=.d}
-DEP_MALLOC := ${subst $(ESP8266_CORE)/umm_malloc,$(OBJDIR),$(DEP_MALLOC)}
-
-DEP_SPIFFS := ${SOURCES_SPIFFS:.cpp=.d}
-DEP_SPIFFS := ${subst $(ESP8266_CORE)/spiffs,$(OBJDIR),$(DEP_SPIFFS)}
-
-DEPS := $(DEP_CORE_CPP) $(DEP_SPIFFS) $(DEP_LIBB64) $(DEP_MALLOC)
--- firmware/ext/core/rules.mk
+++ firmware/ext/core/rules.mk
@@ -6,75 +6,47 @@ Updater_Signing.h:
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 # CORE, c++
 
-$(OBJDIR)/%.o: $(ESP8266_CORE)/%.cpp Updater_Signing.h
+$(OBJDIR)/%.cpp.o: $(ESP8266_CORE)/%.cpp Updater_Signing.h
 	@$(LOG) "(CXX) $@"
 	@$(MKDIR) $(OBJDIR)
 	@$(LOGTIME) $(CXX) $(CFLAGS) $< -o $@ $(LOGONLY)
 	@$(CXX) $(CFLAGS) $< -o $@ $(LOGOUT)
-
-$(OBJDIR)/%.d: $(ESP8266_CORE)/%.cpp
-	@$(LOG) "(CXX) $@"
-	@$(MKDIR) $(OBJDIR)
-	@$(LOGTIME) $(CXX) -MM $(CFLAGS) $< -o $@ $(LOGONLY)
-	@$(CXX) -MM $(CFLAGS) $< -o $@ $(LOGOUT)
-
 
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 # LIBB64, c++
 
-$(OBJDIR)/%.o: $(ESP8266_CORE)/libb64/%.cpp
+$(OBJDIR)/%.cpp.o: $(ESP8266_CORE)/libb64/%.cpp
 	@$(LOG) "(CXX) $@"
 	@$(MKDIR) $(OBJDIR)
 	@$(LOGTIME) $(CXX) $(CFLAGS) $< -o $@ $(LOGONLY)
 	@$(CXX) $(CFLAGS) $< -o $@ $(LOGOUT)
-
-$(OBJDIR)/%.d: $(ESP8266_CORE)/libb64/%.cpp
-	@$(LOG) "(CXX) $@"
-	@$(MKDIR) $(OBJDIR)
-	@$(LOGTIME) $(CXX) -MM $(CFLAGS) $< -o $@ $(LOGONLY)
-	@$(CXX) -MM $(CFLAGS) $< -o $@ $(LOGOUT)
-
 
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 # MALLOC, c++
 
-$(OBJDIR)/%.o: $(ESP8266_CORE)/umm_malloc/%.cpp
+$(OBJDIR)/%.cpp.o: $(ESP8266_CORE)/umm_malloc/%.cpp
 	@$(LOG) "(CXX) $@"
 	@$(MKDIR) $(OBJDIR)
 	@$(LOGTIME) $(CXX) $(CFLAGS) $< -o $@ $(LOGONLY)
 	@$(CXX) $(CFLAGS) $< -o $@ $(LOGOUT)
-
-$(OBJDIR)/%.d: $(ESP8266_CORE)/umm_malloc/%.cpp
-	@$(LOG) "(CXX) $@"
-	@$(MKDIR) $(OBJDIR)
-	@$(LOGTIME) $(CXX) -MM $(CFLAGS) $< -o $@ $(LOGONLY)
-	@$(CXX) -MM $(CFLAGS) $< -o $@ $(LOGOUT)
-
 
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 # SPIFFS, c++
 
-$(OBJDIR)/%.o: $(ESP8266_CORE)/spiffs/%.cpp
+$(OBJDIR)/%.cpp.o: $(ESP8266_CORE)/spiffs/%.cpp
 	@$(LOG) "(CXX) $@"
 	@$(MKDIR) $(OBJDIR)
 	@$(LOGTIME) $(CXX) $(CFLAGS) $< -o $@ $(LOGONLY)
 	@$(CXX) $(CFLAGS) $< -o $@ $(LOGOUT)
 
-$(OBJDIR)/%.d: $(ESP8266_CORE)/spiffs/%.cpp
-	@$(LOG) "(CXX) $@"
-	@$(MKDIR) $(OBJDIR)
-	@$(LOGTIME) $(CXX) -MM $(CFLAGS) $< -o $@ $(LOGONLY)
-	@$(CXX) -MM $(CFLAGS) $< -o $@ $(LOGOUT)
-
-
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 # ASM, c++
 
-$(OBJDIR)/%.o: $(ESP8266_CORE)/%.S Updater_Signing.h $(SOURCES_CORE_ASM)
+$(OBJDIR)/%.S.o: $(ESP8266_CORE)/%.S Updater_Signing.h $(SOURCES_CORE_ASM)
 	@$(LOG) "(GCC) $@"
+	@$(MKDIR) $(OBJDIR)
 	@$(LOGTIME) $(GCC) $(ASMFLAGS) $< -o $@ $(LOGONLY)
 	@$(GCC) $(ASMFLAGS) $< -o $@ $(LOGOUT)
-
 
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 # LIB
