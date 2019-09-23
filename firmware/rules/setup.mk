824690b676cfbc4930dcb259b73b734885db6986
Kai Raphahn <kai.raphahn@laburec.de>
2019-09-23 22:40:18 +0200

    feat: Firmware - Make basics work
    * move flag include
    
    Issue #12

--- firmware/rules/setup.mk
+++ firmware/rules/setup.mk
@@ -12,24 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ESP8266_ROOT     = $(ESP8266_PATH)/tools/xtensa-lx106-elf-gcc/$(XTENSA)
-ESP8266_BIN      = $(ESP8266_ROOT)/bin
-ESP8266_SDK      = $(ESP8266_PATH)/hardware/esp8266/$(ESP_VERSION)/tools/sdk
-ESP8266_CORE     = $(ESP8266_PATH)/hardware/esp8266/$(ESP_VERSION)/cores/esp8266
-ESP8266_VARIANTS = $(ESP8266_PATH)/hardware/esp8266/$(ESP_VERSION)/variants/nodemcu
-ESP8266_SPI      = $(ESP8266_PATH)/hardware/esp8266/$(ESP_VERSION)/libraries/SPI
-ESP8266_EEPROM   = $(ESP8266_PATH)/hardware/esp8266/$(ESP_VERSION)/libraries/EEPROM
-ESP8266_WIFI     = $(ESP8266_PATH)/hardware/esp8266/$(ESP_VERSION)/libraries/ESP8266WiFi/src
+ESP8266_ROOT      = $(ESP8266_PATH)/tools/xtensa-lx106-elf-gcc/$(XTENSA)
+ESP8266_BIN       = $(ESP8266_ROOT)/bin
+ESP8266_SDK       = $(ESP8266_PATH)/hardware/esp8266/$(ESP_VERSION)/tools/sdk
+ESP8266_CORE      = $(ESP8266_PATH)/hardware/esp8266/$(ESP_VERSION)/cores/esp8266
+ESP8266_VARIANTS  = $(ESP8266_PATH)/hardware/esp8266/$(ESP_VERSION)/variants/nodemcu
+ESP8266_SPI       = $(ESP8266_PATH)/hardware/esp8266/$(ESP_VERSION)/libraries/SPI
+ESP8266_EEPROM    = $(ESP8266_PATH)/hardware/esp8266/$(ESP_VERSION)/libraries/EEPROM
+ESP8266_WIFI      = $(ESP8266_PATH)/hardware/esp8266/$(ESP_VERSION)/libraries/ESP8266WiFi/src
+ESP8266_WEBSERVER = $(ESP8266_PATH)/hardware/esp8266/$(ESP_VERSION)/libraries/ESP8266WebServer/src
 
-ESP8266_INCLUDE_SDK      = -I$(ESP8266_SDK)/include -I$(ESP8266_SDK)/lwip2/include -I$(ESP8266_SDK)/libc/xtensa-lx106-elf/include
-ESP8266_INCLUDE_CORE     = -I$(ESP8266_CORE)
-ESP8266_INCLUDE_VARIANTS = -I$(ESP8266_VARIANTS)
-ESP8266_INCLUDE_SPI      = -I$(ESP8266_SPI)
-ESP8266_INCLUDE_EEPROM   = -I$(ESP8266_EEPROM)
-ESP8266_INCLUDE_WIFI     = -I$(ESP8266_WIFI)
 
-ESP8266_LIB              = -L$(ESP8266_SDK)/lib -L$(ESP8266_SDK)/lib/NONOSDK221 -L$(ESP8266_SDK)/ld -L$(ESP8266_SDK)/libc/xtensa-lx106-elf/lib
-ESP8266_INCLUDES         = $(ESP8266_INCLUDE_SDK) $(ESP8266_INCLUDE_CORE) $(ESP8266_INCLUDE_VARIANTS)
+ESP8266_INCLUDE_SDK       = -I$(ESP8266_SDK)/include -I$(ESP8266_SDK)/lwip2/include -I$(ESP8266_SDK)/libc/xtensa-lx106-elf/include
+ESP8266_INCLUDE_CORE      = -I$(ESP8266_CORE)
+ESP8266_INCLUDE_VARIANTS  = -I$(ESP8266_VARIANTS)
+ESP8266_INCLUDE_SPI       = -I$(ESP8266_SPI)
+ESP8266_INCLUDE_EEPROM    = -I$(ESP8266_EEPROM)
+ESP8266_INCLUDE_WIFI      = -I$(ESP8266_WIFI)
+ESP8266_INCLUDE_WEBSERVER = -I$(ESP8266_WEBSERVER)
+
+ESP8266_LIB               = -L$(ESP8266_SDK)/lib -L$(ESP8266_SDK)/lib/NONOSDK221 -L$(ESP8266_SDK)/ld -L$(ESP8266_SDK)/libc/xtensa-lx106-elf/lib
 
 GCC     = $(ESP8266_BIN)/$(ESP8266_GCC_PREFIX)-gcc$(EXEEXT)
 CXX     = $(ESP8266_BIN)/$(ESP8266_GCC_PREFIX)-g++$(EXEEXT)
@@ -51,11 +53,12 @@ LOGGING    = log.txt
 LIB_SUFFIX = .a
 OUTPUT     = build
 
-LIBCORE        = libcore
-LIBSPI         = libspi
-LIBEEPROM      = libeeprom
-LIBESP8266WIFI = libESP8266WiFi
-APP            = sensor
+LIBCORE             = libcore
+LIBSPI              = libspi
+LIBEEPROM           = libeeprom
+LIBESP8266WIFI      = libESP8266WiFi
+LIBESP8266WEBSERVER = libESP8266WebServer
+APP                 = sensor
 
 LOGFILE    = $(ROOT)/$(OUTPUT)/$(LOGGING)
 LOGOUT     = 2>&1 | tee -a $(LOGFILE)
